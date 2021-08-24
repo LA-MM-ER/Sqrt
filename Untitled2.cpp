d951a3035800fe9779c5a3b2274c0619e7b812ad
LA-MM-ER <89412923+LA-MM-ER@users.noreply.github.com>
2021-08-24 21:11:26 +0300

    Add files via upload

--- Untitled2.cpp
+++ Untitled2.cpp
@@ -1,29 +1,34 @@
 #include <iostream>
 #include <cmath>
+#include <stdio.h>
 
 int main()
 {
-    int a;
-    int b;
-    int c;
-    double x;
-    double y;
-    std::cout << "enter a ";
-    std::cin >> a;
-    std::cout << "enter b ";
-    std::cin >> b;
-    std::cout << "enter c ";
-    std::cin >> c;
-    if((b*b-4*a*c) >= 0)
+    double a = 0;
+    double b = 0;
+    double c = 0;
+    double x = 0;
+    double y = 0;
+    printf("enter a, b and c \n");
+    scanf("%lf %lf %lf", &a, &b, &c);
+    if(a == 0)
     {
-        x = ( -1*b + sqrt(b*b - 4*a*c) ) / (2 * a);
-        std::cout << "first root equals " << x;
-        y = ( -1*b - sqrt(b*b - 4*a*c) ) / (2 * a);
-        std::cout << ", second root equals " << y;
+        x = (-1*c / b);
+        printf("root equals %lf" ,x);
     }
-    else
-    {
-        std::cout << "the equation has no real roots";
-    }
+        else
+        {
+            if((b*b-4*a*c) >= 0)
+            {
+                x = ( -1*b + sqrt(b*b - 4*a*c) ) / (2 * a);
+                printf("first root equals %lf" ,x);
+                y = ( -1*b - sqrt(b*b - 4*a*c) ) / (2 * a);
+                printf(", second root equals %lf " ,y);
+            }
+            else
+            {
+                printf("the equation has no real roots");
+            }
+        }
     return 0;
 }
